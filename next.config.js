965fe0b0ac0c8d267856d71853878efc5e56c863
sasidhar <sasidhar.boddeti@indiapost.gov.in>
2025-12-23 14:28:06 +0530

    next config changed

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // IMPORTANT:
-  // 1) Set basePath to your repo name (e.g. '/anu-unicode-converter')
-  // 2) Set assetPrefix to the same value
-  // If you deploy to https://<user>.github.io/<repo>/
-  // Uncomment and edit:
-  basePath: '/anu-unicode-converter',
-  assetPrefix: '/anu-unicode-converter/',
+  output: "export",
   trailingSlash: true,
+  images: { unoptimized: true },
+
+  basePath: "/anu-unicode-converter",
+  assetPrefix: "/anu-unicode-converter/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
